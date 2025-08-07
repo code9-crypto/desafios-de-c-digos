5f3b545e0f4e366f9a1ca4004530ca110040512c
William <convidadoc38@gmai.com>
2025-08-07 12:23:20 -0300

    desafio em java do palíndromo

---
+++ code_challenging/src/palindromo/Execicio1.java
@@ -0,0 +1,28 @@
+package palindromo;
+
+import java.util.Scanner;
+
+public class Execicio1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Digite uma palavra: ");
+		String texto = sc.nextLine();
+		boolean retorno = isPalindrome(texto);
+		System.out.println(retorno);
+	}
+	
+	public static boolean isPalindrome(String texto) {		
+		String resp = "";
+		char[] letras = texto.toCharArray();
+		for( int i=letras.length-1; i>=0; i-- ) {
+			resp += letras[i];
+		}
+		if( texto.equals(resp) ) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+}
