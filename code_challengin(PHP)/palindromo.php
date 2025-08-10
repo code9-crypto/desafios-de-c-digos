f87ee581cc7288cbc2fe658ef0573c25c3ed9d71
code9-crypto <76455073+code9-crypto@users.noreply.github.com>
2025-08-09 23:48:02 -0300

    exercicio do palindromo em php

---
+++ code_challengin(PHP)/palindromo.php
@@ -0,0 +1,10 @@
+<?php
+function isPalindromo($texto){
+    $padroes = ["/\s/", "/\W/"]; //aqui a é um array de expressões regulares; \s -> procura por espaços entre o texto, \W -> procura por pontuações e outros caracteres especiais
+    //$newNome = strtolower(preg_replace($padroes, "", $texto)); -> aqui está trocando os caracteres encontrados no texto por "sem espaçamento"
+    $revTxt = strrev($texto);
+    return $revTxt;
+}
+
+echo isPalindromo("php");
+?>
