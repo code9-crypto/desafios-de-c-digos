fa9483b4bcaa6125e99d7b6db60afb2d9638836a
William <convidadoc38@gmai.com>
2026-07-20 10:34:43 -0300

    criado um desafio onde agrupa os alunos a um grupo e para quem os grupos vai entregar suas atividades

--- code_challengin(Python)/criarGrupos.py
+++ code_challengin(Python)/criarGrupos.py
@@ -2,16 +2,27 @@ import random as rand
 
 #LISTA DE GRUPOS
 geral = []
+numeros_usados = []
 
 for x in range(0,10):
     if len(geral) <= 10:        
         geral.append([])
     
     while len(geral[x]) < 4:
-        geral[x].append(rand.randint(1,41))
+        codigo = rand.randint(1,40)
+        if codigo not in numeros_usados:
+            numeros_usados.append(codigo)
+            geral[x].append(codigo)
+        else:
+            continue
+    
+
 
 print()
 
 #MOSTRANDO OS GRUPOS E SEUS RESPECTIVOS INTEGRANTES
 for grupo, integrantes in enumerate(geral):
-    print(f"Grupo:{grupo+1} são os integrantes: {integrantes}")
+    print(f"Grupo {grupo+1}", end=" -> ")
+    for nome in integrantes:
+        print(nome, end=" ")
+    print("\n")
--- code_challengin(Python)/entregaAtividades.py
+++ code_challengin(Python)/entregaAtividades.py
@@ -1,19 +1,25 @@
 import random as rand
 
 geral = []
+numeros_usados = []
 
 for x in range(0,10):
-    if len(geral) <= 9:
+    if len(geral) <= 20:
         geral.append([])
     
     while len(geral[x]) < 2:
-        if geral[x] == rand.randint(1,10):
-            continue
+        codigo = rand.randint(1,20)
+        if codigo not in numeros_usados:
+            geral[x].append(codigo)
+            numeros_usados.append(codigo)            
         else:
-            geral[x].append(rand.randint(1,10))
+            continue
         
 
 print()
 
 for x, y in enumerate(geral):
-    print(f"Grupo {x+1} => {y[0]} -> {y[1]} ")
+    print(f"Grupo {x+1}", end=" -> ")
+    for senha in y:
+        print(f"{senha}", end=" ")
+    print()
